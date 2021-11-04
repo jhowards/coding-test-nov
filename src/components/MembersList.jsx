71dc353f748878186a656ab8794813f9fef5d4c3
jhowards <github@jameshoward.uk>
2021-11-04 15:11:08 +0000

    Change data order by first name

--- src/components/MembersList.jsx
+++ src/components/MembersList.jsx
@@ -6,12 +6,23 @@ import SingleUser from "./SingleUser";
 const MembersList = () => {
   const [userData, setuserData] = useState();
   const [sortedUserData, setsortedUserData] = useState();
+  const [sortType, setsortType] = useState("a-z");
 
   const fetchData = async () => {
     try {
       const response = await fetch(jsonData);
       let data = await response.json();
       const usersOver18 = data.filter((el) => getAge(el.dateOfBirth) >= 18);
+
+      usersOver18.sort(function (a, b) {
+        if (a.firstName < b.firstName) {
+          return -1;
+        }
+        if (a.firstName > b.firstName) {
+          return 1;
+        }
+        return 0;
+      });
       setuserData(usersOver18);
       setsortedUserData(usersOver18);
     } catch (error) {
@@ -29,23 +40,63 @@ const MembersList = () => {
 
   const handleStatusInput = (e) => {
     if (e.target.value === "all") {
-      setsortedUserData(userData);
+      setsortedUserData(sortData(sortType, userData));
     }
     if (e.target.value === "online") {
-      const sortData = [...userData].filter((el) => el.status === "active");
-      setsortedUserData(sortData);
+      const filterData = [...userData].filter((el) => el.status === "active");
+      const sortFilteredData = sortData(sortType, filterData);
+      setsortedUserData(sortFilteredData);
     }
     if (e.target.value === "away") {
-      const sortData = [...userData].filter((el) => el.status === "away");
-      setsortedUserData(sortData);
+      const filterData = [...userData].filter((el) => el.status === "away");
+      const sortFilteredData = sortData(sortType, filterData);
+      setsortedUserData(sortFilteredData);
     }
     if (e.target.value === "offline") {
-      const sortData = [...userData].filter((el) => el.status === "inactive");
-      setsortedUserData(sortData);
+      const filterData = [...userData].filter((el) => el.status === "inactive");
+      const sortFilteredData = sortData(sortType, filterData);
+      setsortedUserData(sortFilteredData);
     }
   };
 
-  const handleOrderInput = (e) => {};
+  const sortData = (type, data) => {
+    if (type === "a-z") {
+      const sortData = [...data].sort((a, b) => {
+        if (a.firstName < b.firstName) {
+          return -1;
+        }
+        if (a.firstName > b.firstName) {
+          return 1;
+        }
+        return 0;
+      });
+      return sortData;
+    }
+    if (type === "z-a") {
+      const sortData = [...data].sort((a, b) => {
+        if (a.firstName < b.firstName) {
+          return 1;
+        }
+        if (a.firstName > b.firstName) {
+          return -1;
+        }
+        return 0;
+      });
+      return sortData;
+    }
+  };
+
+  const handleOrderInput = (e) => {
+    if (e.target.value === "a-z") {
+      setsortType("a-z");
+      setsortedUserData(sortData("a-z", sortedUserData));
+    }
+
+    if (e.target.value === "z-a") {
+      setsortType("z-a");
+      setsortedUserData(sortData("z-a", sortedUserData));
+    }
+  };
 
   return (
     <div>
