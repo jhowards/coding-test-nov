e6e93ceaaad8aae170437b470164a9d4c1c0e9a5
jhowards <github@jameshoward.uk>
2021-11-04 14:35:06 +0000

    Sort users by status

--- src/components/MembersList.jsx
+++ src/components/MembersList.jsx
@@ -5,12 +5,15 @@ import SingleUser from "./SingleUser";
 
 const MembersList = () => {
   const [userData, setuserData] = useState();
+  const [sortedUserData, setsortedUserData] = useState();
+
   const fetchData = async () => {
     try {
       const response = await fetch(jsonData);
       let data = await response.json();
       const usersOver18 = data.filter((el) => getAge(el.dateOfBirth) >= 18);
       setuserData(usersOver18);
+      setsortedUserData(usersOver18);
     } catch (error) {
       console.log(error);
     }
@@ -24,12 +27,44 @@ const MembersList = () => {
     return (new Date().getTime() - new Date(date).getTime()) / 31536000000;
   };
 
-  console.log(userData);
+  const handleStatusInput = (e) => {
+    if (e.target.value === "all") {
+      setsortedUserData(userData);
+    }
+    if (e.target.value === "online") {
+      const sortData = [...userData].filter((el) => el.status === "active");
+      setsortedUserData(sortData);
+    }
+    if (e.target.value === "away") {
+      const sortData = [...userData].filter((el) => el.status === "away");
+      setsortedUserData(sortData);
+    }
+    if (e.target.value === "offline") {
+      const sortData = [...userData].filter((el) => el.status === "inactive");
+      setsortedUserData(sortData);
+    }
+  };
+
+  const handleOrderInput = (e) => {};
 
   return (
     <div>
-      {userData &&
-        userData.map((user) => (
+      <label>Status</label>
+      <select name="status" id="status" onChange={(e) => handleStatusInput(e)}>
+        <option value="all">All</option>
+        <option value="online">Online</option>
+        <option value="away">Away</option>
+        <option value="offline">Offline</option>
+      </select>
+
+      <label>Order by</label>
+      <select name="order" id="order" onChange={(e) => handleOrderInput(e)}>
+        <option value="a-z">A - Z</option>
+        <option value="z-a">Z - A</option>
+      </select>
+
+      {sortedUserData &&
+        sortedUserData.map((user) => (
           <div key={user.id}>
             <SingleUser user={user} />
           </div>
